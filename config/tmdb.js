57f6643d186efcd5aad194603b2fd21bef17fafb
Dioney Gabriel <62361235+SimpleDioney@users.noreply.github.com>
2025-07-22 09:47:54 -0300

    Update tmdb.js

--- config/tmdb.js
+++ config/tmdb.js
@@ -1,10 +1,7 @@
-// backend/config/tmdb.js
 
 const axios = require('axios');
-// A LINHA ABAIXO FOI CORRIGIDA
 const { default: axiosRetry } = require('axios-retry');
 
-// Criamos uma instância do Axios específica para o TMDB
 const tmdbApi = axios.create({
   baseURL: 'https://api.themoviedb.org/3',
   params: {
@@ -13,18 +10,15 @@ const tmdbApi = axios.create({
   }
 });
 
-// Configuramos o axios-retry
-// Ele vai tentar novamente até 3 vezes se a requisição falhar com um erro de servidor (5xx)
 axiosRetry(tmdbApi, { 
     retries: 3,
     retryDelay: (retryCount) => {
         console.log(`Tentativa de requisição #${retryCount} falhou. Tentando novamente em ${retryCount}s...`);
-        return retryCount * 1000; // 1s, 2s, 3s de espera entre as tentativas
+        return retryCount * 1000; 
     },
     retryCondition: (error) => {
-        // Tenta novamente apenas para erros de rede ou de servidor (5xx)
         if (!error.response) {
-            return true; // Erros de rede (ex: DNS)
+            return true; 
         }
         return error.response.status >= 500;
     }
