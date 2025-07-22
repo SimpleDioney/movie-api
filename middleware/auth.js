004216036920e8994c16f67fbf233738688a4c92
Dioney Gabriel <62361235+SimpleDioney@users.noreply.github.com>
2025-07-22 09:46:45 -0300

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,9 +1,8 @@
-// middleware/auth.js
 const jwt = require('jsonwebtoken');
 
 function authMiddleware(req, res, next) {
     const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1]; // Formato "Bearer TOKEN"
+    const token = authHeader && authHeader.split(' ')[1]; 
 
     if (token == null) {
         return res.status(401).json({ message: 'Acesso não autorizado: Token não fornecido.' });
@@ -14,7 +13,6 @@ function authMiddleware(req, res, next) {
             return res.status(403).json({ message: 'Acesso proibido: Token inválido.' });
         }
         
-        // Adiciona os dados do usuário (do token) ao objeto da requisição
         req.user = user;
         next();
     });
